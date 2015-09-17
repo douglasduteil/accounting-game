ea3588355c824dfe3bb58f5845c86d8bc968df2d
Douglas Duteil <douglasduteil@gmail.com>
2015-09-17 22:47:18 +0200

    chore(dep): update

--- config.js
+++ config.js
@@ -15,37 +15,54 @@ System.config({
   },
 
   map: {
-    "angular": "github:angular/bower-angular@1.4.5",
-    "angular-animate": "github:angular/bower-angular-animate@1.4.5",
+    "angular": "github:angular/bower-angular@1.4.6",
+    "angular-animate": "github:angular/bower-angular-animate@1.4.6",
     "angular-ui-router": "github:angular-ui/ui-router@0.2.15",
     "angular-youtube-embed": "github:brandly/angular-youtube-embed@1.1.0",
-    "babel": "npm:babel-core@5.8.23",
-    "babel-runtime": "npm:babel-runtime@5.8.20",
+    "babel": "npm:babel-core@5.8.24",
+    "babel-runtime": "npm:babel-runtime@5.8.24",
     "core-js": "npm:core-js@0.9.18",
     "lodash": "npm:lodash@3.10.1",
     "text": "github:systemjs/plugin-text@0.0.2",
     "videogular": "github:2fdevs/bower-videogular@1.2.6",
     "github:2fdevs/bower-videogular@1.2.6": {
-      "angular": "github:angular/bower-angular@1.4.5",
-      "angular-sanitize": "github:angular/bower-angular-sanitize@1.4.5"
+      "angular": "github:angular/bower-angular@1.4.6",
+      "angular-sanitize": "github:angular/bower-angular-sanitize@1.4.6"
     },
     "github:angular-ui/ui-router@0.2.15": {
-      "angular": "github:angular/bower-angular@1.4.5"
+      "angular": "github:angular/bower-angular@1.4.6"
     },
-    "github:angular/bower-angular-animate@1.4.5": {
-      "angular": "github:angular/bower-angular@1.4.5"
+    "github:angular/bower-angular-animate@1.4.6": {
+      "angular": "github:angular/bower-angular@1.4.6"
     },
-    "github:angular/bower-angular-sanitize@1.4.5": {
-      "angular": "github:angular/bower-angular@1.4.5"
+    "github:angular/bower-angular-sanitize@1.4.6": {
+      "angular": "github:angular/bower-angular@1.4.6"
     },
     "github:brandly/angular-youtube-embed@1.1.0": {
-      "angular": "github:angular/bower-angular@1.4.5"
+      "angular": "github:angular/bower-angular@1.4.6"
     },
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
-    "npm:babel-runtime@5.8.20": {
+    "github:jspm/nodelibs-util@0.1.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:amdefine@1.0.0": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "module": "github:jspm/nodelibs-module@0.1.0",
+      "path": "github:jspm/nodelibs-path@0.1.0",
       "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:assert@1.3.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:babel-runtime@5.8.24": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:buffer@3.5.0": {
+      "base64-js": "npm:base64-js@0.0.8",
+      "ieee754": "npm:ieee754@1.1.6",
+      "is-array": "npm:is-array@1.0.1"
     },
     "npm:core-js@0.9.18": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
